fd28710cb6fb2e6e648a09aa5034b299691adb5d
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-11-11 16:25:22 +0100

    finished

--- app/src/main/java/com/example/revisionbeginningandroid3/Theme10.java
+++ app/src/main/java/com/example/revisionbeginningandroid3/Theme10.java
@@ -3,12 +3,45 @@ package com.example.revisionbeginningandroid3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
-public class Theme10 extends AppCompatActivity {
-
+public class Theme10 extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+    RadioGroup orientation;
+    RadioGroup gravity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme10);
+        orientation=(RadioGroup)findViewById(R.id.orientation);
+        orientation.setOnCheckedChangeListener(this);
+        gravity=(RadioGroup)findViewById(R.id.gravity);
+        gravity.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+        switch (checkedId) {
+            case R.id.horizontal:
+                orientation.setOrientation(LinearLayout.HORIZONTAL);
+                break;
+
+            case R.id.vertical:
+                orientation.setOrientation(LinearLayout.VERTICAL);
+                break;
+            case R.id.left:
+                gravity.setGravity(Gravity.LEFT);
+                break;
+
+            case R.id.center:
+                gravity.setGravity(Gravity.CENTER_HORIZONTAL);
+                break;
+
+            case R.id.right:
+                gravity.setGravity(Gravity.RIGHT);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/revisionbeginningandroid3/Theme9.java
+++ app/src/main/java/com/example/revisionbeginningandroid3/Theme9.java
@@ -3,12 +3,41 @@ package com.example.revisionbeginningandroid3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Theme9 extends AppCompatActivity {
+public class Theme9 extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme9);
+        EditText fld=(EditText)findViewById(R.id.editText);
+        fld.setText("Licensed under the Apache License, Version 2.0 " +
+                "(the \"License\"); you may not use this file " +
+                "except in compliance with the License. You may " +
+                "obtain a copy of the License at " +
+                "http://www.apache.org/licenses/LICENSE-2.0");
+
+        cb=(CheckBox)findViewById(R.id.check);
+        cb.setOnCheckedChangeListener(this);
+    }
+
+    public void buttonMethod(View view) {
+        Toast.makeText(this, "This is the result", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b) {
+            cb.setText("This checkbox is: checked");
+        }
+        else {
+            cb.setText("This checkbox is: unchecked");
+        }
     }
 }
